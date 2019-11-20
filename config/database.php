d4bd60cdccd9bffa03c3dd28de1efc033403ad78
lijunqian <lijq@mutantbox.com>
2019-11-20 09:52:23 +0800

    optimize

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '10.8.189.203'),
             'port' => env('DB_PORT', 3306),
             'database' => env('DB_DATABASE', 'october'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'dbmanger'),
+            'password' => env('DB_PASSWORD', 'edJelrZuAgpNeLyn'),
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => '',
