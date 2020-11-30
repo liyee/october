8a39720e85580f568979062d535f273d77663ed4
lijunqian <lijq@mutantbox.com>
2020-11-30 15:12:20 +0800

    异常判断

--- index.php
+++ index.php
@@ -38,15 +38,21 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 */
 $pool = ['127.0.0.1', '220.248.33.174'];
 $ip = $_SERVER['REMOTE_ADDR'];
+$ip = '999.999.999.99';
 if (!in_array($ip, $pool)){
-    $reader = new \GeoIp2\Database\Reader("Migration/GeoLite2-Country.mmdb");
-    $record = $reader->country($ip);
-    $isoCode = $record->country->isoCode;
-    if ($isoCode == "CN"){
-        echo "<div style=\"font-size: large;\">Please contact the administrator!</div>";
-        //header("HTTP/1.0 404 Not Found");
-        exit();
+    try {
+        $reader = new \GeoIp2\Database\Reader("Migration/GeoLite2-Country.mmdb");
+        $record = $reader->country($ip);
+        $isoCode = $record->country->isoCode;
+        if ($isoCode == "CN"){
+            echo "<div style=\"font-size: large;\">Please contact the administrator!</div>";
+            //header("HTTP/1.0 404 Not Found");
+            exit();
+        }
+    }catch (Exception $e){
+        //echo $e->getMessage();die;
     }
+
 }
 $kernel = $app->make('Illuminate\Contracts\Http\Kernel');
 
